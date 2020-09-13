42da7aef563cbc0c30c07ae9c06f8730a441a23f
Shigehiro IDANI <idani@hirotae.com>
2020-09-13 12:11:20 +0900

    support MPA router TomVista's change & ktn's comment applied

--- src/webpack-plugin.ts
+++ src/webpack-plugin.ts
@@ -6,21 +6,31 @@ import { generateRoutes, GenerateConfig } from 'vue-route-generator'
 
 const pluginName = 'VueAutoRoutingPlugin'
 
-interface Options extends GenerateConfig {}
+interface Options extends GenerateConfig {
+  pageName?: string
+}
 
 namespace VueAutoRoutingPlugin {
   export type AutoRoutingOptions = Options
 }
 
 class VueAutoRoutingPlugin {
-  constructor(private options: Options) {
-    assert(options.pages, '`pages` is required')
+  constructor(private options: Options | Options[]) {
+    if (Array.isArray(options)) {
+      options.forEach(option => {
+        assert(option.pages, '`pages` is required')
+        assert(option.pageName, '`pageName` is required')
+      })
+    } else {
+      assert(options.pages, '`pages` is required')
+    }
   }
 
   apply(compiler: Compiler) {
-    const generate = () => {
-      const code = generateRoutes(this.options)
-      const to = path.resolve(__dirname, '../index.js')
+    const generate = (option: Options) => {
+      const code = generateRoutes(option)
+      const pageName = option.pageName ? option.pageName : 'index'
+      const to = path.resolve(__dirname, `../${pageName}.js`);
 
       if (
         fs.existsSync(to) &&
@@ -32,9 +42,37 @@ class VueAutoRoutingPlugin {
       fs.writeFileSync(to, code)
     }
 
+    const generateIndex = (options: Options[]) => {
+      const importCode:string[] = [];
+      const exportCode:string[] = [];
+      options.forEach(option => {
+        const to = path.resolve(__dirname, `../${option.pageName}.js`);
+        if (fs.existsSync(to)){
+          importCode.push(`import ${option.pageName} from './${option.pageName}';`)
+          exportCode.push(`${option.pageName}`)
+        }
+      })
+      return `${importCode.join("\n")}export{${exportCode.join(',')}}`
+    }
+
     compiler.hooks.thisCompilation.tap(pluginName, (compilation) => {
       try {
-        generate()
+        if (typeof this.options === 'object') {
+          generate(this.options as Options);
+        } else if (Array.isArray(this.options)) {
+          this.options.forEach(option => {
+            generate(option)
+          })
+          const indexPath = path.resolve(__dirname, '../index.js');
+          const code = generateIndex(this.options)
+          if (fs.existsSync(indexPath) &&
+            fs.readFileSync(indexPath, 'utf8').trim() === code.trim()) {
+            return;
+          }
+          fs.writeFileSync(indexPath, code);
+        } else {
+          throw new Error('options must be an object or an array')
+        }
       } catch (error) {
         compilation.errors.push(error)
       }
